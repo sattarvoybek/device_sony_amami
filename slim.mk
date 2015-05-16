6e512a91294299dee10acbc5ab067e2252ac363e
sattarvoybek <sattarvoybek@gmail.com>
2015-05-16 11:36:09 +0300

    Update slim.mk

--- slim.mk
+++ slim.mk
@@ -12,16 +12,27 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/sony/amami/full_amami.mk)
+$(call inherit-product, device/zte/nx404h/full_nx404h.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := amami
+PRODUCT_RELEASE_NAME := nx404h
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := amami
-PRODUCT_NAME := slim_amami
-PRODUCT_BRAND := Xperia
-PRODUCT_MODEL := Z1 Compact
-PRODUCT_MANUFACTURER := Sony
+PRODUCT_DEVICE := nx404h
+PRODUCT_NAME := slim_nx404h
+PRODUCT_BRAND := zte
+PRODUCT_MODEL := Z5s Mini
+PRODUCT_MANUFACTURER := zte
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.4.4/14.4.A.0.133/k___jQ:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.4.4 14.4.A.0.133 k___jQ release-keys"
+PRODUCT_DEFAULT_LANGUAGE := ru
+PRODUCT_DEFAULT_REGION := RU
+
+PRODUCT_LOCALES := ru_RU ua_UK en_US
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=NX404H BUILD_FINGERPRINT="nubia/NX404H/NX404H:4.3/JLS36C/eng.nubia.20140810.194729:user/release-keys" PRIVATE_BUILD_DESC="NX404H-user 4.3 JLS36C eng.nubia.20140810.194729 release-keys"
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=nx404h \
+    TARGET_DEVICE=nx404h \
+    PRODUCT_MODEL=nx404h
